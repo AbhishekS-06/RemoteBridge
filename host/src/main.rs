1e5acbb39591cd549caa9df604f0db70f319a2ea
Abhishek Shrestha <theshresthaabhishek@gmail.com>
2026-09-13 21:30:02 -0500

    Capture frames from main display with ScreenCaptureKit

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use screencapturekit::prelude::*;
+
+const CAPTURE_WIDTH: u32 = 1280;
+const CAPTURE_HEIGHT: u32 = 720;
+
+struct Handler;
+impl SCStreamOutputTrait for Handler {
+    fn did_output_sample_buffer(&self, sample: CMSampleBuffer, _: SCStreamOutputType) {
+        println!("frame @ {:?}", sample.presentation_timestamp());
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let content = SCShareableContent::get()?;
+    let display = &content.displays()[0];
+
+    let filter = SCContentFilter::create()
+        .with_display(display)
+        .with_excluding_windows(&[])
+        .build();
+
+    let config = SCStreamConfiguration::new()
+        .with_width(CAPTURE_WIDTH)
+        .with_height(CAPTURE_HEIGHT)
+        .with_pixel_format(PixelFormat::BGRA);
+
+    let mut stream = SCStream::new(&filter, &config);
+    stream.add_output_handler(Handler, SCStreamOutputType::Screen);
+    stream.start_capture()?;
+
+    std::thread::sleep(std::time::Duration::from_secs(5));
+    stream.stop_capture()?;
+    Ok(())
 }
